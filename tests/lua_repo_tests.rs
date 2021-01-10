3c900a48fe61dec4a9a5de7d41d00f0221549fa0
Robert Masen <robert.masen@smartthings.com>
2021-01-10 13:30:15 -0600

    remove unused in test

--- tests/lua_repo_tests.rs
+++ tests/lua_repo_tests.rs
@@ -1,6 +1,7 @@
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
 use analisar::{Error, Parser};
+use log::info;
 
 #[test]
 fn try_all() {
@@ -34,11 +35,10 @@ fn try_all() {
             eprintln!("trying {}", entry.path().display());
             let text = std::fs::read(entry.path()).unwrap();
             let mut t = Parser::new(text.as_slice());
-            let mut chunk_ct = 0;
             while let Some(chunk) = t.next() {
                 match chunk {
-                    Ok(_c) => {
-                        //println!("{:?}", c)
+                    Ok(c) => {
+                        info!("{:?}", c)
                     },
                     Err(Error::UnexpectedToken(offset, msg)) => {
                         if let Some(line) = analisar::error::find_line(&text, offset) {
@@ -48,8 +48,8 @@ fn try_all() {
                                 line.number,
                                 line.offset
                             );
-                            eprintln!(
-                                "{}\n{}\n{}\n{}",
+                            let msg = format!(
+                                "\n{}\n{}\n{}\n{}",
                                 path,
                                 String::from_utf8_lossy(
                                     &text[line.start..line.start + line.length]
@@ -57,39 +57,14 @@ fn try_all() {
                                 "^".repeat(line.length),
                                 msg
                             );
-                            panic!()
+                            panic!("{}", msg);
                         } else {
                             panic!("{}-{}", offset, msg);
                         }
                     }
                     Err(Error::UnexpectedEof) => panic!("Unexpected EOF"),
                 }
-                chunk_ct += 1;
             }
         }
     }
-}
-
-fn try_read(p: impl AsRef<Path>) -> String {
-    let bytes = std::fs::read(p.as_ref()).unwrap_or_else(|e| {
-        panic!(
-            "failed to read file as bytes {}: {}",
-            p.as_ref().display(),
-            e
-        )
-    });
-    let mut s = String::with_capacity(bytes.len());
-    for (idx, line) in bytes.split(|b| *b == b'\n').enumerate() {
-        match String::from_utf8(line.to_vec()) {
-            Ok(l) => {
-                s.push_str(&l);
-                s.push('\n');
-            }
-            Err(_) => {
-                println!("Invalid utf8 {}:{}:0", p.as_ref().display(), idx + 1)
-            }
-        }
-    }
-
-    s
 }
